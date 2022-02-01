77274b52a23cf2ca9c2754df5398e367d03c42b4
Shinurr <97344163+Shinurr@users.noreply.github.com>
2022-02-01 07:21:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -258,6 +258,18 @@ new_string = " ".join(new_ml_string)
 
     return await addFooter(new_string)
 
+
+
+
+
+
+
+
+
+
+
+
+
 async def new_Doodstream_url(urls):
 
     new_urls = []
@@ -275,6 +287,14 @@ async def remove_username(new_List):
     for i in new_List:
 
         if('@' in i or 't.me' in i or 'https://bit.ly/abcd' in i or 'https://bit.ly/123abcd' in i or 'telegra.ph' in i):
+
+ 
+
+
+
+
+
+
 
             new_List.remove(i)
 
